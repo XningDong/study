64222ff5a8b1300c26af7fb44b34932382b7d6e1
dongxinning <dongxinning@192.168.31.240>
2018-06-12 08:19:10 +0900

    bug test

---
+++ mavenStudy/src/main/java/com/mavenStudy/service/UserService.java
@@ -0,0 +1,11 @@
+package com.mavenStudy.service;
+
+public class UserService {
+	public String getUserName() {
+		String nullStr = null;
+		if(nullStr.equals("a")) {
+			//thi is a bug
+		}
+		return "";
+	}
+}
