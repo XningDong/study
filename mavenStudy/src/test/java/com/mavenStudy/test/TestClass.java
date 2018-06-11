7f3c7b14b3bd0b9e709aa8d996ef137a178c1953
dongxinning <dongxinning@192.168.31.240>
2018-06-11 20:41:47 +0900

    the frist commit

---
+++ mavenStudy/src/test/java/com/mavenStudy/test/TestClass.java
@@ -0,0 +1,9 @@
+package com.mavenStudy.test;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		System.out.println("hello sonarqube!"); //this is a class for study sonarqube
+	}
+
+}
